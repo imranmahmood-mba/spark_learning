72ed349339869649d9880198c05f2f870ac6239c
imranmahmood-mba <im6979@rit.edu>
2023-08-02 20:00:55 -0700

    made updates

--- PythonScripts/flight-delays.py
+++ PythonScripts/flight-delays.py
@@ -12,7 +12,7 @@ schema = StructType([
     StructField("date", DateType(), True),
         StructField("delay", IntegerType()), 
         StructField("distance", IntegerType()),
-        StructField("string", StringType()), 
+        StructField("origin", StringType()), 
         StructField("destination", StringType())
 ])
  # Create DataFrame
@@ -21,8 +21,9 @@ df = spark.read.csv("../LearningSparkV2/databricks-datasets/learning-spark-v2/fl
 # test grouping
 sum_destination_grouped_df = df.groupBy("destination").sum("delay")
 avg_destination_grouped_df = df.groupBy("destination").agg(F.avg("delay").alias("avg_delay")).withColumn("avg_delay", F.round(F.col("avg_delay"), 2))
-
+sum_longest_trip_df = df.groupBy(["origin", "destination"]).agg(F.avg("delay").alias("avg_delay")).withColumn("avg_delay", F.round(F.col("avg_delay"), 2))
 
 # Displays the content of the DataFrame to stdout
 sum_destination_grouped_df.show()
 avg_destination_grouped_df.show()
+sum_longest_trip_df.orderBy(["origin", "destination"]).show()
