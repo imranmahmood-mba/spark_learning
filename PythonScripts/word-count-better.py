4c8f7292924aba40d1cac98a261ae20674bf5585
imranmahmood-mba <im6979@rit.edu>
2023-08-05 12:36:01 -0700

    added txt file, changed file path

--- PythonScripts/word-count-better.py
+++ PythonScripts/word-count-better.py
@@ -7,11 +7,12 @@ def normalizeWords(text):
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
-input = sc.textFile("file:///sparkcourse/book.txt")
+input = sc.textFile("../wreckedonthefeejees.txt")
 words = input.flatMap(normalizeWords)
 wordCounts = words.countByValue()
-
 for word, count in wordCounts.items():
     cleanWord = word.encode('ascii', 'ignore')
     if (cleanWord):
         print(cleanWord.decode() + " " + str(count))
+
+
